2b41b2b94075ef57019ee01a2853418822b83eb9
Satguru P Srivastava <ssatguru@gmail.com>
2017-05-09 21:55:29 -0500

    removed redundant scene files

--- public_html/worlds.js
+++ public_html/worlds.js
@@ -1,14 +1,8 @@
-var worlds={
-		"demo":{
-			"worldFolder":"demos",
-			"sceneFolder":"demo1",
-			"sceneFile":"demo",
-			"editEnabled":true
-		},
-		"demo2":{
-			"worldFolder":"demos",
-			"sceneFolder":"demo1",
-			"sceneFile":"demo2",
-			"editEnabled":true
-		}
+var worlds = {
+	"demo1": {
+		"worldFolder": "demos",
+		"sceneFolder": "demo",
+		"sceneFile": "demo1",
+		"editEnabled": true
+	}
 };

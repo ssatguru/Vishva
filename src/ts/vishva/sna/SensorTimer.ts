8775b50c59b83b7522b1a05266f37decae6c46a0
Satguru P Srivastava <ssatguru@gmail.com>
2017-12-03 16:43:21 -0600

    added property plusMinus to  randomize interval

--- src/ts/vishva/sna/SensorTimer.ts
+++ src/ts/vishva/sna/SensorTimer.ts
@@ -1,29 +1,30 @@
 namespace org.ssatguru.babylonjs.vishva {
 
-    import AbstractMesh = BABYLON.AbstractMesh;
-    import Action = BABYLON.Action;
-    import ActionManager = BABYLON.ActionManager;
-    import ExecuteCodeAction = BABYLON.ExecuteCodeAction;
-    import Mesh = BABYLON.Mesh;
-    import Scene = BABYLON.Scene;
-    import Tags = BABYLON.Tags;
+    import AbstractMesh=BABYLON.AbstractMesh;
+    import Action=BABYLON.Action;
+    import ActionManager=BABYLON.ActionManager;
+    import ExecuteCodeAction=BABYLON.ExecuteCodeAction;
+    import Mesh=BABYLON.Mesh;
+    import Scene=BABYLON.Scene;
+    import Tags=BABYLON.Tags;
 
     export class SenTimerProp extends SNAproperties {
-        interval : number = 1000;
+        interval: number=1000;
+        plusMinus: number=0;
         public unmarshall(obj: Object): SenTimerProp {
             return <SenTimerProp>obj;
         }
     }
-    
+
     export class SensorTimer extends SensorAbstract {
-        
-        timerId:number;
-        
-        public constructor(mesh: Mesh, prop: SenTimerProp) {
-           if (prop!=null){
-                super(mesh, prop);
-            }else{
-                super(mesh, new SenTimerProp());
+
+        timerId: number;
+
+        public constructor(mesh: Mesh,prop: SenTimerProp) {
+            if(prop!=null) {
+                super(mesh,prop);
+            } else {
+                super(mesh,new SenTimerProp());
             }
             this.processUpdateSpecific();
         }
@@ -37,21 +38,49 @@ namespace org.ssatguru.babylonjs.vishva {
         }
 
         public setProperties(properties: SNAproperties) {
-            this.properties =  <SenTimerProp> properties;
+            this.properties=<SenTimerProp>properties;
         }
 
         public cleanUp() {
-            window.clearInterval(this.timerId);
+            //window.clearInterval(this.timerId);
+            window.clearTimeout(this.timerId);
         }
 
-        public processUpdateSpecific() {
-            let properties : SenTimerProp = <SenTimerProp> this.properties;
-            if (this.timerId){
+        public processUpdateSpecific_old() {
+            let properties: SenTimerProp=<SenTimerProp>this.properties;
+            if(this.timerId) {
                 window.clearInterval(this.timerId);
             }
-            this.timerId = window.setInterval(() => {this.emitSignal();}, properties.interval);
+            this.timerId=window.setInterval(() => {this.emitSignal();},properties.interval);
         }
-      
-    }            
+        public processUpdateSpecific() {
+            let properties: SenTimerProp=<SenTimerProp>this.properties;
+            if(this.timerId) {
+                window.clearTimeout(this.timerId);
+            }
+            this.timerId=window.setTimeout(() => {this.signalEmitter();},properties.interval);
+        }
+
+        private signalEmitter() {
+
+            let properties: SenTimerProp=<SenTimerProp>this.properties;
+            this.emitSignal();
+            window.clearTimeout(this.timerId);
+            let i: number=properties.interval;
+            let pm: number=properties.plusMinus;
+            if(pm>0) {
+                let min: number=i-pm;
+                if(min<0) min=0;
+                i=this.getRandomInt(min,i+pm);
+            }
+            this.timerId=window.setTimeout(() => {this.signalEmitter();},i)
+        }
+        private getRandomInt(min,max): number {
+            min=Math.ceil(min);
+            max=Math.floor(max);
+            return Math.floor(Math.random()*(max-min+1))+min; //The maximum is inclusive and the minimum is inclusive 
+        }
+
+    }
 }
 org.ssatguru.babylonjs.vishva.SNAManager.getSNAManager().addSensor("Timer",org.ssatguru.babylonjs.vishva.SensorTimer);
